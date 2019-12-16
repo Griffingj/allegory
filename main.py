e42c611b4cf95bdb4b4154a25fab2e02250fbaf5
John Griffing <johngriffing@GriffingjMBPro2016.fios-router.home>
2019-12-16 02:12:40 -0500

    Make chess.html root

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/')
+def root():
+    return app.send_static_file('chess.html')
